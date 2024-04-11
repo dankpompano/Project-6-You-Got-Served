14b1185957d5fe927f2ed9a669758c26a2520e72
joeldjustice <163780260+joeldjustice@users.noreply.github.com>
2024-04-11 13:07:46 -0400

    Update webserver.c got socket

--- webserver.c
+++ webserver.c
@@ -8,63 +8,6 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-int main(int argc, char **argv)
-{
-	unsigned short port = 0;
-	int serverSocket = 0;
-	int clientSocket = 0;
-	char buffer[1024];
-	int length = 0;
-	int value = 1;
-	struct sockaddr_in address;
-	struct sockaddr_storage otherAddress;
-	socklen_t otherSize = sizeof (otherAddress);
-	
-	// Read port from user
-	printf ("Enter port: ");
-	scanf ("%hu", &port);
-
-	// Make socket
-	serverSocket = socket (AF_INET, SOCK_STREAM, 0);
-	
-	// Make it able to reuse ports	
-	setsockopt (serverSocket, SOL_SOCKET, SO_REUSEADDR, &value, sizeof(value));
-
-	// Bind socket to port and local IP
-	memset (&address, 0, sizeof(address));
-	address.sin_family = AF_INET;
-	address.sin_port = htons(port);
-	address.sin_addr.s_addr = INADDR_ANY;
-	bind (serverSocket, (struct sockaddr*)&address, sizeof(address));
-
-	// Listen
-	listen (serverSocket, 1);
-	
-	// Accept	
-	clientSocket = accept (serverSocket, (struct sockaddr *) &otherAddress, &otherSize);	
-
-	printf ("\n\n*Chat Started*\n\n");
-	
-	// Receive messages
-	while ((length = read( clientSocket, buffer, sizeof(buffer) - 1)) > 0)
-	{		
-		buffer[length] = '\0';
-		if (strcmp (buffer, "quit") == 0)
-			break;
-			
-		printf ("%s\n", buffer);
-	}	
-
-	printf ("\n*Chat Ended*\n");
-	
-	// Clean up
-	close (clientSocket );
-	close (serverSocket );
-	
-	return 0;
-}
-
-
 int main(int argc, int **argv)
 {
 	int port = argv[1];
@@ -84,6 +27,15 @@ int main(int argc, int **argv)
 	}
 	
 	//get socket
-	
+	int sockFD = socket(AF_INET, SOCK_STREAM, 0);
+	struck sockaddr_in address;
+	memset(&address, 0, sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_port = htons(port);
+	address.sin_addr.s_addr = INADDR_ANY;
+	bind(sockFD, (struck sockaddr*) &address, sizeof(address));
+	int value = 1;
+	setsockopt(sockFD, SOL_SOCKET, SOLSO_REUSEADDR, &value, sizeof(value));
+	listen(sockFD, 1);
 
 }
